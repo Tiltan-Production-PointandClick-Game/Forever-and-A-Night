5fcd1e638850cb60b79e8234756db9a6316a3121
Eddy Izakson <eddyizakson@gmail.com>
2019-06-29 18:28:15 +0300

    cutscene rework!!
    simple interactions, moving between scenes

---
+++ Forever and A Night/Assets/Scripts/AnimatedText.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatedText : MonoBehaviour
+{
+    [SerializeField]
+    bool clickToProceed;
+
+    [SerializeField]
+    float fadeTime; // amount of time it takes to fade an image
+
+    //Time taken for each letter to appear (The lower it is, the faster each letter appear)
+    public float letterPaused = 0.01f;
+    //Message that will displays till the end that will come out letter by letter
+    public string message;
+    //Text for the message to display
+    public Text textComp;
+
+    [SerializeField]
+    public string newTxt;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get text component
+        textComp = GetComponent<Text>();
+        //Message will display will be at Text
+        message = textComp.text;
+        //Set the text to be blank first
+        textComp.text = "";
+        //Call the function and expect yield to return
+        StartCoroutine(TypeText());
+
+    }
+
+    IEnumerator TypeText()
+    {
+        //Split each char into a char array
+        foreach (char letter in message.ToCharArray())
+        {
+            //Add 1 letter each
+            textComp.text += letter;
+            yield return 0;
+            yield return new WaitForSeconds(letterPaused);
+        }
+    }
+
+    public IEnumerator ChangeText()
+    {
+        WaitForMouseClick();
+
+        if (message.Length == message.Length + 1)
+        {
+            textComp.text = "";
+            message = newTxt;
+            yield return StartCoroutine(TypeText());
+        }
+    }
+
+
+    IEnumerator WaitForMouseClick()
+    {
+        while (!Input.GetMouseButton(0))
+            yield return null;
+    }
+}
